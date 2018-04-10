ea398c11eea8f925d1ef81617669a8c7f4d779b9
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-04-10 20:33:23 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,5 @@ class Song < ActiveRecord::Base
   validates :title, presence: true
   validates :released, inclusion: {in: %w(true false)}
   validates :artist_name, presence: true
-  validate :release_year, presence: true, if: :released? 
+  validates :release_year, presence: true, if: :released? 
 end
