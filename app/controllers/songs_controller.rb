38d5bef5457926d79edc8d113ba827ab90fecc80
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-04-10 20:11:08 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,25 +1,26 @@
 class SongsController < ApplicationController
 
-  def index 
-  end 
+  def index
+    @songs = Song.all 
+  end
 
-  def new 
-  end 
+  def new
+  end
 
-  def create 
-  end 
+  def create
+  end
 
-  def show 
-  end 
+  def show
+  end
 
-  def edit 
-  end 
+  def edit
+  end
 
-  def update 
-  end 
+  def update
+  end
 
-  def delete 
-  end 
+  def delete
+  end
 
-  
+
 end
