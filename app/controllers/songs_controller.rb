6f7c63ebff673548ee426bdd2276c290a4d509d3
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-04-10 20:12:03 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,13 +1,21 @@
 class SongsController < ApplicationController
 
   def index
-    @songs = Song.all 
+    @songs = Song.all
   end
 
   def new
+    @song = Song.new
   end
 
   def create
+    @song = Song.new(song_params)
+    if @song.valid?
+      @song.save
+      redirect_to song_path(@song)
+    else
+      render :new
+    end 
   end
 
   def show
