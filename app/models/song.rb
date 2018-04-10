b46bebcff4a2f405987de35cc5ce2c87c3e178bf
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-04-10 20:42:25 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,6 @@
 class Song < ActiveRecord::Base
   validates :title, presence: true
-  validates :title, uniqueness: true
+  validates :title, uniqueness: {scope: [:release_year, :artist_name], message: "cannot be repeated by the same artist in the same year."}
   validates :released, inclusion: {in: [true, false]}
   validates :artist_name, presence: true
   validates :release_year, presence: true, if: :released?
